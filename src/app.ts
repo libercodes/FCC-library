a8905e41ed00e66f7a1ca555aa14e290de1c5e3d
Liber <libermenna@gmail.com>
2020-05-30 22:06:44 -0300

    Added helmet for security

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import { ApplicationRun } from './controller/System'
 import userRouter from './routes/user'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
 dotenv.config()
 
 const app: Application = express()
 app.use(cors({origin: '*'})); 
+app.use(helmet())
 //app.use(crossOriginMiddleware)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
